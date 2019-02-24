c00bdde3cc8c597efd451d5ff3ade857931aa194
bpoep72 <bpoep72@gmail.com>
2019-02-24 17:20:30 -0500

    Working on reading the header

--- Classifiers/ImageReader.py
+++ Classifiers/ImageReader.py
@@ -1,29 +1,31 @@
 # -*- coding: utf-8 -*-
 
+
+
 class ImageReader:
     
     #where the specific bands we plan to use is presently defined
     bands_file_path = "../Resources/bands.txt"
+    #head to the images directory
     image_dir = "../Images/"
     
     def __init__(self, imageFile):
         self.image_file = imageFile
         
+        
     def __del__(self):
         pass
-
-    #Get the header file if it exists
+    
+    #Read the header file if it exists
     #Params: none
     #Return: the header file
     #Throws: FileNotFoundError if file does not exist
-    def get_Header(self):
+    def readin_header(self):
         header = self.image_file + ".hdr"
        
-        try:
-            header_file = open(header, 'r')
-            header_file.close()
-        except FileNotFoundError:
-            raise FileNotFoundError("Image File is missing its corresponding header.")
+        with open(self.image_dir + header) as header_file:
+            pass
+
 
     #Get the specific bands that we work with for this project so that the 
     #can be loaded according to these bands
@@ -42,5 +44,15 @@ class ImageReader:
             
         #return the bands as floats
         return bands
+    
+    def get_original_image(self):
+        pass
+        
+    #Get the image using the specific bands that are defiend in bands.txt
+    #this should be the default.
+    #Params: none
+    #Return: 
+    def get_image(self):
+        pass
     
     
