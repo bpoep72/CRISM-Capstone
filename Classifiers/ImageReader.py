d1676241eee186531a35cb99f448ac5f2d0087ba
bpoep72 <bpoep72@gmail.com>
2019-02-25 12:21:54 -0500

    Finished reading the header files.

--- Classifiers/ImageReader.py
+++ Classifiers/ImageReader.py
@@ -1,36 +1,112 @@
 # -*- coding: utf-8 -*-
 
-
+#Read in the image from the .img file format where there is a seperate .hdr
+#file that outlines the contents of the .img file. This reader's principle task
+#is to take these two files and import only the wavelengths that we need, these
+#are defined in /Resources/bands.txt. Bands and wavelength are interchanged
+#frequently in this file, sorry.
 
 class ImageReader:
     
-    #where the specific bands we plan to use is presently defined
+    #where the specific bands we plan to use is defined
     bands_file_path = "../Resources/bands.txt"
     #head to the images directory
     image_dir = "../Images/"
     
     def __init__(self, imageFile):
         self.image_file = imageFile
-        
+        self.header_bands = self.get_header_wavelengths()
+        self.specific_bands = self.get_bands()
+        self.ignore_value = self.get_header_data_ignore_value()
+        self.matched_bands = self.match_bands()
         
     def __del__(self):
         pass
     
-    #Read the header file if it exists
+    #Match the bands that came from the bands.txt file to the bands from the
+    #header file. Make an array of the int indices of the bands in header_bands
     #Params: none
-    #Return: the header file
+    #Return: int[]: the indices in header_bands of the values that matches
+    #           a value from the specific bands
+    def match_bands(self):
+        
+        matched_bands = []
+        
+        #run over the list of the bands from the header file to find the indices
+        #of matches
+        for i in range(len(self.specific_bands)):
+            #j will never been less than i but it can be equal so save some time
+            j = i
+            #scroll through the header_bands till a match is found
+            while(self.specific_bands[i] != self.header_bands[j]):
+                j += 1
+            #at this point j should be a match
+            matched_bands.append(j)
+            
+        return matched_bands
+    
+    #Read the wavelengths from the header file for the original image
+    #Params: none
+    #Return: float[]: the wavelength that are used in the original image
     #Throws: FileNotFoundError if file does not exist
-    def readin_header(self):
+    def get_header_wavelengths(self):
         header = self.image_file + ".hdr"
        
+        #get the wavelenghts from the header file
         with open(self.image_dir + header) as header_file:
-            pass
+            file = header_file.read()
+            
+            #get the index where the waves start at
+            word = "wavelength = {"
+            start_index = file.find(word) + len(word) + 1
+            
+            #get the place where the waves end
+            word = "}"
+            end_index = file.find(word, start_index)
+            
+            #get the bands from the header file
+            header_bands = file[start_index:end_index]
+            
+            #get rid of end the end line character
+            header_bands = header_bands.replace("\n", "")
+            #split the csv file
+            header_bands = header_bands.split(", ")
+            
+            #convert to float
+            for i in range(len(header_bands)):
+                header_bands[i] = float(header_bands[i])
+            
+            return header_bands
 
+    #The header has a field where it tells of a value the instrument assigns
+    #to a pixel value whenever the data be ignored. This function finds that
+    #value.
+    #Params: none
+    #Return: string: the ignore value in scientific notation
+    #Throws: FileNotFoundError if file does not exist
+    def get_header_data_ignore_value(self):
+        header = self.image_file + ".hdr"
+       
+        #get the data ignore value from the header file
+        with open(self.image_dir + header) as header_file:
+            file = header_file.read()
+            
+            #get the index where the data ignore value is at
+            word = "data ignore value = "
+            start_index = file.find(word) + len(word)
+            
+            word = "\n"
+            end_index = file.find(word, start_index)
+            
+            ignore_value = file[start_index:end_index]
+            
+            return ignore_value
 
     #Get the specific bands that we work with for this project so that the 
-    #can be loaded according to these bands
+    #image can be loaded according to these bands.
     #Params: none
-    #Return: float[] bands
+    #Return: float[]: the bands from bands.txt
+    #Throws: FileNotFoundError if file does not exist
     def get_bands(self):
         #open the resource file
         bands_file = open(self.bands_file_path, 'r')
@@ -45,14 +121,29 @@ class ImageReader:
         #return the bands as floats
         return bands
     
+    #Read in the original image using all the original bands
+    #Params: none
+    #Return:
     def get_original_image(self):
-        pass
+        
+        with open(self.image_dir + self.image_file):
+            pass
         
     #Get the image using the specific bands that are defiend in bands.txt
     #this should be the default.
     #Params: none
     #Return: 
     def get_image(self):
-        pass
+        
+        with open(self.image_dir + self.image_file):
+            pass
+    
+    
+    
+    
+    
+    
+    
+    
     
     
