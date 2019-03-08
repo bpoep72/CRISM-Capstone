1658fb770f18b68ad92426f3aa659ff5e8d2a07a
bpoep72 <bpoep72@gmail.com>
2019-03-08 10:02:54 -0500

    Added a test and method to CRISMImage

--- classifiers/CRISMImage.py
+++ classifiers/CRISMImage.py
@@ -38,23 +38,31 @@ class CRISMImage:
     '''
     def get_ignore_matrix(self):
         
-        for i in range(self.dimensions):
-            pass
+        #for each pixel find if it contains the data ignore value
+        for i in range(self.rows):
+            for j in range(self.columns):
+                pixel = self.get_pixel_vector(i, j)
+                
         
     '''
         The goal of this method is to get a single band based on its index from
         the bands array
         NOTE: This uses the new array not the old one so an operation like 
-        imshow(image, [band, band, band]) will not use the same bands as this
+        imshow(image, [band, band, band]) from the matlab file will not use 
+        the same bands as this. to translate pass each of those bands to self.get_new_ref()
     ''' 
     def get_band_by_number(self, band_number):
 
-        if(band_number == -1) or (band_number > len(self.bands)):
+        if(band_number < 0) or (band_number > len(self.bands)):
             raise Exception("Invalid band number given to get_band_by_number() of Class CRISMImage")
         
         band_matrix = self.raw_image[:, :, band_number]
 
         return band_matrix
+
+    def get_pixel_vector(self, row, column):
+
+        return self.raw_image[row, column, :]
     
     def get_band_by_wavelength(self, wave_length):
         
@@ -68,7 +76,12 @@ class CRISMImage:
         Return: int - the index of the band in self.bands, -1 if not found
     '''
     def get_band_index(self, wave_length):
-        
+
+        try:
+            index = self.bands.index(wave_length)
+            return index
+        except ValueError:
+            return -1
         return self.bands.index(wave_length)
 
     '''
@@ -81,8 +94,12 @@ class CRISMImage:
     def get_new_ref(self, band):
         
         wave_length = self.original_bands[band]
-        
-        return self.bands.index(wave_length)
+
+        try:
+            referenced_index = self.bands.index(wave_length)
+            return referenced_index
+        except ValueError:
+            return -1
 
 
 if __name__ == "__main__":
--- test/test_CRISMImage.py
+++ test/test_CRISMImage.py
@@ -3,6 +3,7 @@ import unittest
 import classifiers
 import os.path
 import sys
+#add the root of the project to the system path 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 from classifiers.crismimage import CRISMImage
@@ -10,6 +11,25 @@ from classifiers.imagereader import ImageReader
 
 class test_CRISMImage(unittest.TestCase):
 
+    #the image from which all oracle data was derived for these tests
     image = "HRL000040FF_07_IF183L_TRR3_BATCH_CAT_corr.img"
 
-    pass
+    '''
+        Objective: Ensure that CRISMImage.get_new_ref() can convert an old band reference
+        to a new one if it exists and returns -1 otherwise
+    '''
+    def test_get_new_ref(self):
+        
+        imr = ImageReader(self.image)
+
+        image = imr.get_raw_image()
+
+        #old ref should be not exist in the new image
+        value = image.get_new_ref(1)
+        self.assertEqual(value, -1)
+
+        #old ref should exist in the new image
+        value = image.get_new_ref(3)
+        self.assertEqual(value, 0)
+
+    
