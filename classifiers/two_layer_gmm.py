Working to correct precision issue

classifiers/two_layer_gmm.py
@@ -9,6 +9,11 @@ from classifiers.imagereader import ImageReader
 import math
 import numpy
 import scipy.special
+import scipy.linalg
+
+'''
+    Use a student T distribution to make a prediction about the input image based on the model files
+'''
 
 class Two_Layer_Gmm:
 
@@ -16,11 +21,12 @@ class Two_Layer_Gmm:
     def __init__(self, image, model):
         self.image = image
         self.data = model
+        self.normalized_image = self.norm_image()
 
         #outputs
         self.ypred = None
         self.aa = None
-        self.loglik = None
+        self.bb = None
     
     def test(self):
 
@@ -28,20 +34,63 @@ class Two_Layer_Gmm:
         d = len(data.mu_s[0])
 
         pi_constant = (d / 2) * math.log(math.pi)
-        g1_pc = scipy.special.gammaln( numpy.arange(.5, numpy.max(20), .5 ) ) #NOTE This works at execution have no idea why vscode tells me it won't
-        n = len(self.image[0])
+        g1_pc = scipy.special.gammaln( numpy.arange(.5, numpy.max(self.data.v_s[:]) + d, .5 ) )
+        n = len(self.normalized_image)
         loglik = numpy.zeros((n, ncl))
         
         for i in range(ncl):
-            v = self.image - self.data.mu_s[i]
-            chsig = numpy.linalg.cholesky( data.Sig_s[:, :, i] )
+            v = self.normalized_image - self.data.mu_s[i, :]
+            chsig = numpy.linalg.cholesky( self.data.Sig_s[:, :, i] ).T
+
+            x = g1_pc[ self.data.v_s[i] + d ]
+            y = g1_pc[ self.data.v_s[i] ] + (d/2) * math.log( self.data.v_s[i] )
+            z = numpy.sum( numpy.log(numpy.diagonal(chsig)))
            
-            tpar = g1_pc( data.v_s[i] + d ) - ( g1_pc( data.v_s[i] ) + (d/2) * math.log( data.v_s[i] ) + pi_constant ) - numpy.sum( math.log(numpy.diagonal(chsig)))
+            tpar = x - ( y + pi_constant ) - z
 
-            temp = mrdivide(v, chsig) #TODO: need to convert this to python still can't find a method that works
+            temp = numpy.linalg.lstsq(chsig, v.T, rcond=None)[0].T
 
-            loglik[: i] = tpar - .5 * (data.v_s[i] + d) * math.log(1 + ( 1 / data.v_s[i] )) * numpy.sum( numpy.multiply(temp, temp)[2] )
+            a = (self.data.v_s[i] + d)
+            b = numpy.sum( numpy.multiply(temp, temp), axis=1)
+            c = numpy.log(1 + ( 1 / self.data.v_s[i] ) * b)
 
+            loglik[:, i] = tpar - .5 * a * c
+
+        #find the max across the rows
+        self.aa = numpy.amax(loglik, axis=1)
+        #find where the max occurs
+        self.bb = numpy.argmax(loglik, axis=1)
+        #predict that the index of the max of each row is associated with a class id
+        self.ypred = self.data.class_id[self.bb]
+
+        '''
+        Normalizes each pixel to be length 1. Store the resulting matrix into self.normalized_image.
+        We store it there because it messes with the display of the image if we did it anywhere else.
+        Also we only need this to run the normalized image through the two layer gmm. We only used 50
+        channels for this step. Given that the two layer gmm test expects the image to be in the shape
+        where each pixel is a row we do that here.
+
+        Params: None
+        Returns: float[][], the normalized image where each row is a single pixel vector
+
+    '''
+    def norm_image(self):
+
+        #init an empty matrix
+        normalized_image = numpy.zeros( (self.image.rows * self.image.columns, self.data.fin.size) )
+
+        #reshape the image such that each pixel is a row in the image variable
+        image = numpy.reshape( self.image.raw_image, (self.image.rows * self.image.columns, self.image.dimensions) )
+
+        #remove the columns that we don't need according to fin
+        reduced_image = image[:, self.data.fin]
+
+        row_norms = numpy.linalg.norm(reduced_image, axis=2)
+
+        for i in range(len(row_norms)):
+            normalized_image[i] = numpy.divide(reduced_image[i, :], row_norms[i])
+
+        return normalized_image
 
 if __name__ == "__main__":
     
@@ -59,5 +108,5 @@ if __name__ == "__main__":
     data = npzFileReader.read_file(resource_file)
 
     #TODO: image is still in the original matrix form need to transform it so each row is a pixel and each column is a dimension
-    Two_Layer_Gmm(img.raw_image, data).test()
+    Two_Layer_Gmm(img, data).test()
         