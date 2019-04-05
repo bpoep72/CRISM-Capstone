a339a6fd59a44b8b3ea686ac7a23857fc2252642
bpoep72 <bpoep72@gmail.com>
2019-04-05 17:00:09 -0400

    Added a method for testing the gui

--- classifiers/classificationmap.py
+++ classifiers/classificationmap.py
@@ -11,13 +11,16 @@ class ClassificationMap:
 
     def __init__(self, predicted_labels):
         
+        #the predictions from the classifier double[][]
         self.predications = predicted_labels
 
+        #the unique predicted labels int[]
         self.labels = numpy.unique(self.predications)
 
+        #the individial layers afer get_map is run, Layer[]
         self.layers = [None]*len(self.labels)
 
-        self.get_map()
+        self.make_map()
 
     '''
         Produces the layered classification map
@@ -26,7 +29,7 @@ class ClassificationMap:
         Return: None
         Side Effect: self.layers is now initialized
     '''
-    def get_map(self):
+    def make_map(self):
 
         for label in range(len(self.labels)):
 
@@ -81,6 +84,24 @@ class ClassificationMap:
             b = color_val - (rgb_max * 2)
 
         return r, g, b
+
+    '''
+        A method designed just for testing the gui. This will generate dummy data
+        formatted as output from the classifier when fed the rows, columns and a number
+
+        Params:
+            rows, int, a number of rows to generate random predicitions for
+            columns, int, a number of columns to generate random predictions for
+            num_labels, int, the number of labels/layers to generate
+    '''
+    def make_random_map(self, rows, columns, num_labels):
+
+        combined_map = numpy.random.randint(0, num_labels, size=(rows, columns))
+        self.labels = numpy.arange(0, 25)
+        self.layers = [None]*len(self.labels)
+        self.predications = combined_map
+
+        self.make_map()
         
 
 if __name__ == "__main__":
@@ -88,6 +109,7 @@ if __name__ == "__main__":
     test_mat = numpy.random.rand(15, 15)
 
     classification_map = ClassificationMap(test_mat)
+    classification_map.make_random_map(310, 450, 25)
 
     print()
 
